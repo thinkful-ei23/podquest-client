174b271237fb68d2a306da5a22f7c0fbcaa509d0
Shelly Leung <pontificable@yahoo.com>
2018-10-29 14:13:54 -0700

    fetch works, mapped out response

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,5 +4,34 @@ export const getPodcasts = searchTerm => dispatch => {
 	return fetch(`${ITUNES_API}/search?term=${searchTerm}&entity=podcast`, {
 		method: 'GET',
 		headers: { 'Content-Type': 'application/json' }
-	}).then(res => console.log(res));
+	})
+		.then(res => {
+			if (!res.ok) {
+				if (
+					res.headers.has('content-type') &&
+					res.headers.get('content-type').startsWith('application/json')
+				) {
+					console.log(res.json());
+					return res.json().then(err => Promise.reject(err));
+				}
+				return Promise.reject({
+					code: res.status,
+					message: res.statusText
+				});
+			}
+			return res.json();
+		})
+		.then(res => {
+			let response = res.results.map(result => {
+				return {
+					id: result.collectionId,
+					collection: result.collectionName,
+					xml: result.feedUrl,
+					image: result.artworkUrl100
+				};
+			});
+			//keep artistId, collectionName, feedUrl, artworkUrl100
+
+			console.log(response);
+		});
 };
--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-function SearchForm(props){
-    let input;
-    return(
-        <form onSubmit={(e)=>{
-            e.preventDefault();
-            console.log(' insert fetch action here: ', input.value)}} >
-            <label htmlFor='search-input'>Search: </label>
-            <input id='search-input' placeholder='Title,Description,etc.' ref={search =>input = search }/>
-            <button className='search-button' >Search</button>
-        </form>
-    );
-};
+export default function SearchForm(props) {
+	let input;
+	return (
+		<form
+			onSubmit={e => {
+				e.preventDefault();
+				props.onSubmit(input.value);
+			}}
+		>
+			<label htmlFor="search-input">Search: </label>
+			<input
+				id="search-input"
+				placeholder="Title,Description,etc."
+				ref={search => (input = search)}
+			/>
+			<button className="search-button">Search</button>
+		</form>
+	);
+}
 
-
-export default connect()(SearchForm);
+// export default connect()(SearchForm);
