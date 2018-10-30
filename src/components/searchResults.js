7364e6bc8bab60d0dba99a5bc04d4c5ed3c1fa4f
john <johnjacobcosgrove@gmail.com>
2018-10-30 14:00:41 -0400

    fixed search result display

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -3,11 +3,14 @@ import {Link} from 'react-router-dom'
 
 export default function SearchResults(props){
         return(
-            <Link
-                to={{
-                    pathname:`/channel/${props.podcast.id}` }}>
-                {props.podcast.collection}
-            </Link>
+            <div>
+                <Link
+                    to={{
+                        pathname:`/channel/${props.podcast.id}` }}>
+                    {props.podcast.collection}
+                </Link>
+            </div>
+            
         )
 }
 
