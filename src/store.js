7817310a5cf6528a21fcb79785c76f991d55773b
Clinton Owen <clint@clintonowen.com>
2018-10-29 22:04:31 -0500

    Add actions/reducers to get channel info from XML/RSS

--- src/store.js
+++ src/store.js
@@ -4,13 +4,15 @@ import thunk from 'redux-thunk';
 import {loadAuthToken} from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
+import searchReducer from './reducers/search';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
 
 const store = createStore(
     combineReducers({
         form: formReducer,
         auth: authReducer,
-        protectedData: protectedDataReducer
+        protectedData: protectedDataReducer,
+        search: searchReducer
     }),
     applyMiddleware(thunk)
 );
