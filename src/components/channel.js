7b4f51d108db5f0ef1464d090e5f47d7b14c1fac
Chris Gantz <chrisgantz836@gmail.com>
2018-10-31 08:07:06 -0700

    bug in channel also added fixed height

--- src/components/channel.js
+++ src/components/channel.js
@@ -2,21 +2,32 @@ import React from 'react';
 import {connect} from 'react-redux'
 
 class Channel extends React.Component{
+    
 
     render(){
-        console.log('props', this.props);
+        console.log('props', this.props); // see podcasts
+        if (!this.props.podcasts) {
+            return <div>Loading...</div>;
+        }
+        const podcast = this.props.podcasts.find(podcast =>  {
+            // console.log(typeof podcast.id)  // Type of is different so we used just ==
+            // console.log('this.props.match.params.id', typeof this.props.match.params.id);
+            return podcast.id === Number(this.props.match.params.id)
+        })
+        console.log('podcast', podcast);
         return(
             <div>
-                Hello Channel
+                <span>{podcast.collection}</span>
+                <img src={podcast.image} alt="podcast wallpaper" height={200}/>
             </div>
         )
     }
 }
 
 const mapStateToProps = state => {
-    console.log('state', state);
+    console.log('state', state); // to look at state
     return {
-        image: state.search.podcasts
+        podcasts: state.search.podcasts
     }
 }
 
