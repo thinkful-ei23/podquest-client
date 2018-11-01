c304686b37c0f6e8d177e879e7cce2382ab2f0ab
kathyleboeuf <kleboeuf@townisp.com>
2018-11-01 15:54:12 -0400

    fontAwesome className added

--- src/components/about.js
+++ src/components/about.js
@@ -19,12 +19,12 @@ export class About extends React.Component {
 		return (
 			<div className="about-app box">
 
-				<h2>Are you on a <i id="world" class="fas fa-globe-americas"></i><span>quest</span> to find that specific podcast episode?</h2>
+				<h2>Are you on a <i id="world" className="fas fa-globe-americas"></i><span>quest</span> to find that specific podcast episode?</h2>
 				<p>Blah &nbsp;&#126; &nbsp;blah&nbsp; ~~blah&nbsp;It's time to put on makeup. It's time to dress up right. It's time to
 				raise the curtain on the Muppet Show tonight. Sunny Days sweepin' the clouds away. On my way to where the air is sweet. Can you tell me how to
 				get how to get to Sesame Street.</p>
 				<h3>Look no further!</h3>
-				<p>Well, actually, you'll need to look a little further.&nbsp;<i class="far fa-smile-wink"></i></p>
+				<p>Well, actually, you'll need to look a little further.&nbsp;<i className="far fa-smile-wink"></i></p>
 				<h4>But, we're here to help!</h4>
 
 				<p className="about-directions">
