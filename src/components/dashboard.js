159c6a9055da5da9cd658de3d7532f493616de25
kathyleboeuf <kleboeuf@townisp.com>
2018-11-01 18:25:41 -0400

    trying to fix dash.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,9 +4,8 @@ import requiresLogin from './requires-login';
 import SearchForm from './searchForm';
 import { fetchProtectedData } from '../actions/protected-data';
 import { getPodcasts } from '../actions/search';
-import SearchResults from './searchResults';
-import './dashboard.css';
 import ShowResults from './showResults';
+import './dashboard.css';
 
 export class Dashboard extends React.Component {
 	constructor(props) {
@@ -27,31 +26,6 @@ export class Dashboard extends React.Component {
 		});
 	}
 
-	handlePrev(e) {
-		// dispatch the same GET from iTunes or a new action?
-		// requires the original input query + attr (if attr exists)
-		// current offset + 1, current offset starts at 2
-		console.log('Prev button clicked');
-		const input = e.target.value;
-		this.setState(
-			{
-				offset: this.state.offset - 10
-			},
-			() => {
-				this.state.selectedOption
-					? this.props.dispatch(
-						getPodcasts(input, this.state.selectedOption, this.state.offset)
-					)
-					: this.props.dispatch(getPodcasts(input, '', this.state.offset));
-			}
-		);
-		// console.log('Prev', this.state.offset);
-		// this.state.selectedOption
-		// 	? this.props.dispatch(
-		// 			getPodcasts(input, this.state.selectedOption, this.state.offset)
-		// 	  )
-		// 	: this.props.dispatch(getPodcasts(input, '', this.state.offset));
-	}
 
 	handleNext(e) {
 		// dispatch the same GET from iTunes or a new action?
@@ -73,53 +47,11 @@ export class Dashboard extends React.Component {
 			}
 		);
 	}
-	// handlePrev(e) {
-	// 	// dispatch the same GET from iTunes or a new action?
-	// 	// requires the original input query + attr (if attr exists)
-	// 	// current offset + 1, current offset starts at 2
-	// 	console.log('Prev button clicked');
-	// 	const input = e.target.value;
-	// 	this.setState(
-	// 		{
-	// 			offset: this.state.offset - 10
-	// 		},
-	// 		() => {
-	// 			this.state.selectedOption
-	// 				? this.props.dispatch(
-	// 						getPodcasts(input, this.state.selectedOption, this.state.offset)
-	// 				  )
-	// 				: this.props.dispatch(getPodcasts(input, '', this.state.offset));
-	// 		}
-	// 	);
-	// }
 
-	// handleNext(e) {
-	// 	// dispatch the same GET from iTunes or a new action?
-	// 	// requires the original input query + attr (if attr exists)
-	// 	// current offset + 1, current offset starts at 2
-	// 	console.log('Next button clicked');
-	// 	const input = e.target.value;
-	// 	this.setState(
-	// 		{
-	// 			offset: this.state.offset + 10
-	// 		},
-	// 		() => {
-	// 			// console.log('Next', this.state.offset);
-	// 			this.state.selectedOption
-	// 				? this.props.dispatch(
-	// 						getPodcasts(input, this.state.selectedOption, this.state.offset)
-	// 				  )
-	// 				: this.props.dispatch(getPodcasts(input, '', this.state.offset));
-	// 		}
-	// );
-	// }
 
 	onSubmit(e) {
-		// console.log(e);
 		if (this.state.selectedOption) {
-			// console.log('option is', this.state.selectedOption);
 			this.props.dispatch(getPodcasts(e, this.state.selectedOption));
-			// .then(() => this.SearchForm.reset());
 		} else {
 			this.props.dispatch(getPodcasts(e));
 		}
@@ -172,7 +104,6 @@ export class Dashboard extends React.Component {
 
 const mapStateToProps = state => {
 	const { currentUser } = state.auth;
-	// console.log(state.search.podcasts);
 	return {
 		username: state.auth.currentUser.username,
 		name: `${currentUser.name} `,
