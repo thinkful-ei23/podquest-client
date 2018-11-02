e96f505f8dbac8bb15009c47c86e7d0d3aa2bc1e
john <johnjacobcosgrove@gmail.com>
2018-11-02 11:26:18 -0400

    added some end to end tests

---
+++ cypress/integration/login.js
@@ -0,0 +1,9 @@
+describe('podQuest - Login', function() {
+    it('Visits the site and logs in', function() {
+        cy.visit('http://localhost:3000')
+        cy.contains('Login').click()
+        cy.get('input#username').type('there you2')
+        cy.get('input#password').type('password123')
+        cy.get('button.login-button').click()
+    })
+  })
