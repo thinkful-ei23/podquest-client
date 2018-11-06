21e978e590c02b0fcf76f5b7398b1c876b87fcdf
john <johnjacobcosgrove@gmail.com>
2018-11-06 13:23:39 -0500

    adding assertions

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -2,9 +2,11 @@ describe('podQuest - Login', function() {
     it('Visits the site and logs in', function() {
         cy.visit('/')
         cy.contains('Login').click()
+        cy.url().should('include','/login')
         cy.get('input#username').type('there you2').should('have.value','there you2')
         cy.get('input#password').type('password123').should('have.value','password123')
         cy.get('button.login-button').click()
+        cy.url().should('include','/dashboard')
     });
     it('Visits the site and logs in then logs out', function() {
         cy.visit('/')
@@ -13,6 +15,7 @@ describe('podQuest - Login', function() {
         cy.get('input#password').type('password123').should('have.value','password123')
         cy.get('button.login-button').click()
         cy.get('.btn-logout').click()
+        cy.url().should('include','/')
     });
     it('Visits the site and searchs for podcasts', function() {
         cy.visit('/')
@@ -20,14 +23,15 @@ describe('podQuest - Login', function() {
         cy.get('input#username').type('there you2').should('have.value','there you2')
         cy.get('input#password').type('password123').should('have.value','password123')
         cy.get('button.login-button').click()
-        cy.get('input#search-input').type('joe').type('{enter}')
+        cy.get('input#search-input').type('joe').should('have.value','joe').type('{enter}')
     });
     it('Selects a podcast and clicks to listen', function() {
         cy.visit('/')
-        cy.contains('Login').click()
-        cy.get('input#username').type('there you2').should('have.value','there you2')
-        cy.get('input#password').type('password123').should('have.value','password123')
-        cy.get('button.login-button').click()
+        // cy.contains('Login').click()
+        // cy.get('input#username').type('there you2').should('have.value','there you2')
+        // cy.get('input#password').type('password123').should('have.value','password123')
+        // cy.get('button.login-button').click()
+        cy.request('POST', '/api/auth/login', { username: 'there you2',password:'password123' }).its('body').as('currentUser')
         cy.get('input#search-input').type('code').type('{enter}')
         cy.contains('Code Switch').click()
         cy.get('#episode-select').select('Word Up')
