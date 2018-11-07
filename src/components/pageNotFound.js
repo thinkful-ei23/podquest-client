968bb559edbb8e0a0aa1bbae761b4a659d964824
john <johnjacobcosgrove@gmail.com>
2018-11-07 16:12:44 -0500

    styled 404 page alittle

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function PageNotFound(props){
     return(
         <div className ='pageNotFound'>
-            <p>404<br/> no page Exists here</p>
-            <p>Please select one of these options</p>
+            <h3>404<br/> no page Exists here</h3>
+            <h4>Please select one of these options</h4>
         </div>
     )
 }
