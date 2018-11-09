2ba42a00f2fd81e8ba7eaefd47b320260876ed8e
Shelly Leung <pontificable@yahoo.com>
2018-11-09 12:28:31 -0800

    react table works

---
+++ src/components/showTable.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactTable from 'react-table';
+import { Link } from 'react-router-dom';
+
+import 'react-table/react-table.css';
+
+export default class ResultTable extends React.Component {
+	render() {
+		const data = this.props.podcasts.map(result => ({
+			collection: {
+				collectionName: result.collection,
+				xml: result.xml
+			}
+		}));
+		// console.log(data);
+
+		const columns = [
+			{
+				Header: 'Your search yielded',
+				accessor: 'collection',
+				Cell: ({ row }) => (
+					<Link
+						onClick={() =>
+							localStorage.setItem('podcastChannel', row.collection.xml)
+						}
+						to={{
+							pathname: `/channel`
+						}}
+					>
+						{row.collection.collectionName}
+					</Link>
+				)
+			}
+		];
+
+		return (
+			<div>
+				<ReactTable data={data} columns={columns} />
+			</div>
+		);
+	}
+}
